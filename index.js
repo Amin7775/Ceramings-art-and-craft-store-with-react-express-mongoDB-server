3d1d22b2f3406ab909aa47407ce29a023de8f37d
Amin7775 <amin.dpi24@gmail.com>
2024-04-30 00:59:47 +0600

    Connected to mongodb, installed dotenv to use environment variables and sucure user informations

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000 ; 
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// db start
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@clusterpherob9.3leb5bl.mongodb.net/?retryWrites=true&w=majority&appName=ClusterPheroB9`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // operations
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+// db end
 
 app.get('/', (req,res)=>{
     res.send("Ceramings Server is running");
